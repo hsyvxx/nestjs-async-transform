edc2aa41593189d039a645c43d40c5b79db8f176
hsyvxx <hsyvxx@gmail.com>
2020-08-05 02:18:39 +0900

    add AsyncTransformPipe to index.ts

--- libs/nestjs-async-transform/src/index.ts
+++ libs/nestjs-async-transform/src/index.ts
@@ -1,2 +1,3 @@
 export { AsyncTransformOption } from './async-transform-option';
 export { AsyncTransform } from './async-transform.decorator';
+export { AsyncTransformPipe } from './async-transform.pipe';
