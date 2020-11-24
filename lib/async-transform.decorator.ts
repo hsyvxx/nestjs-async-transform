749ffc44acc33dbad3be53510f06c80b14064b3f
hsyvxx <hsyvxx@gmail.com>
2020-11-24 14:45:09 +0900

    fix

--- lib/async-transform.decorator.ts
+++ lib/async-transform.decorator.ts
@@ -1,4 +1,4 @@
-import { ASYNC_TRANSFORM_METADATA, ASYNC_TRANSFORM_NESTED } from './constants';
+import { ASYNC_TRANSFORM_METADATA } from './constants';
 import { Expose, ExposeOptions } from 'class-transformer';
 import { AsyncTransformOption } from './async-transform-option';
 
@@ -6,6 +6,5 @@ export const AsyncTransform = <T = unknown, R = unknown, E extends Error = Error
   option: AsyncTransformOption<T, R, E> & { exposeOptions?: ExposeOptions },
 ): PropertyDecorator => (target, propertyKey) => {
   Expose(option.exposeOptions)(target, propertyKey as string);
-  if (option.type) Reflect.defineMetadata(ASYNC_TRANSFORM_NESTED, option.type(), target, propertyKey);
   Reflect.defineMetadata(ASYNC_TRANSFORM_METADATA, option, target, propertyKey);
 };
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,4 @@
 export const ASYNC_TRANSFORM_METADATA = 'ASYNC_TRANSFORM_METADATA';
-export const ASYNC_TRANSFORM_NESTED = 'ASYNC_TRANSFORM_NESTED';
 
 export const MESSAGES = {
   m001: 'Circular dependence was found.',
